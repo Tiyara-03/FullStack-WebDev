ecd4cf995be571f9eeb2124d75328775eac01236
Tiyara-03 <tiara26dia37@gmail.com>
2024-02-12 17:09:02 +0530

    Update index.js

--- devtown/index.js
+++ devtown/index.js
@@ -1,3 +1,11 @@
 const fs = require("fs");
 
 fs.copyFileSync("first.txt" , "second.txt");
+
+const express = require("express");
+const App = express();
+
+App.get("/", (req,res)=>
+    res.send("Hello World"));
+App.listen(3000);
+
