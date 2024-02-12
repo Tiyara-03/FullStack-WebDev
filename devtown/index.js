3b458108df6180c073c8933879470cd8e7ef1421
Tiyara-03 <tiara26dia37@gmail.com>
2024-02-12 22:52:38 +0530

    more on nodejs

--- devtown/index.js
+++ devtown/index.js
@@ -3,9 +3,19 @@ const fs = require("fs");
 fs.copyFileSync("first.txt" , "second.txt");
 
 const express = require("express");
+const bodyParser = require("body-parser")
 const App = express();
 
+App.use(bodyParser.urlencoded({extended:true}));
+
 App.get("/", (req,res)=>
-    res.send("Hello World"));
-App.listen(3000);
+    res.sendFile(__dirname + "/index.html"));
+
+App.post("/", (req, res)=>{
+    var n1 = Number(req.body.n1);
+    var n2 = Number(req.body.n2);
+    var result = n1+ n2;
+    res.send("The answer is" + result);
+} );
+App.listen(3000 ,() => console.log("The server has started running on port 3000 "));
 
