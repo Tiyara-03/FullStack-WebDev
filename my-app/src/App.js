289b121d24b5aeddf0992bd4d75a7db54e8acfde
Tiyara-03 <tiara26dia37@gmail.com>
2024-05-10 18:35:55 +0530

    more in react.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,16 @@
 // import logo from './logo.svg';
 import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm';
+import About from './components/About.js';
+
 function App() { 
   return ( 
     <>
       <Navbar title="TextUtils" aboutText="about us"/>
       <div className="container mb-3">
       <TextForm heading="Enter the text to analyze below"/>
+      <About />
 
       </div>
     </>
--- my-app/src/components/TextForm.js
+++ my-app/src/components/TextForm.js
@@ -18,6 +18,18 @@ export default function TextForm(props) {
         // console.log("Onchange");
         setText(event.target.value); //to add values in the text-box
   }
+
+  const handleCopy =()=>{
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraSpaces = ()=>{
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "))
+  }
+
     const[text, setText] = useState('enter text here');
     // setText("new text");
     return (
@@ -29,6 +41,10 @@ export default function TextForm(props) {
             </div>
             <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to uppercase</button>
             <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button className="btn btn-primary mx-2"  onClick={handleCopy}>Copy</button>
+            <button className="btn btn-primary mx-2"  onClick={handleExtraSpaces}>handleExtraSpaces</button>
+
+
         </div>
         <div className="container my-4">
             <h1>Your text summary</h1>
