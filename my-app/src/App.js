e97cd4be96a6166df1357ea3891caa82fdd9893e
Tiyara-03 <tiara26dia37@gmail.com>
2024-05-11 14:26:13 +0530

    states in react.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import Navbar from './components/Navbar.js';
 import TextForm from './components/TextForm';
 import About from './components/About.js';
+import { useState } from 'react';
+
+
 
 function App() { 
+  const [mode] = useState('light'); //Whether dark mode is enables or not
   return ( 
     <>
-      <Navbar title="TextUtils" aboutText="about us"/>
+      <Navbar title="TextUtils" aboutText="about us" mode={mode}/>
       <div className="container mb-3">
       <TextForm heading="Enter the text to analyze below"/>
       <About />
