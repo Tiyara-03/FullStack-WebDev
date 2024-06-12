5878ecc73484da4a85b47ffbd5927e00c9b8cb20
Tiyara-03 <tiara26dia37@gmail.com>
2024-06-12 09:22:20 +0530

    states in react.js

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -3,13 +3,32 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Textform from './components/Textform';
 import About from './components/About';
+import { useState } from 'react';
+import Alert from './components/Alert';
 
 function App() {
+  const [ mode, setMode] = useState('light');
+  const[alert, setAlert] = useState(null);
+
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode('dark')
+      document.body.style.backgroundColor = "rgb(24 35 45)";
+
+    }
+    else{
+      setMode('light')
+      document.body.style.backgroundColor = "white";
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtils"/> 
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils"/>  */}
+      <Navbar title="textUtils" mode={mode} toggleMode = {toggleMode}/>
+      <Alert alert="This is alert" />
       <div className="container my-3">
-      <Textform heading=" Enter the text to analyse below"/>
+      <Textform heading=" Enter the text to analyse below" mode={mode}/>
       </div>
       <About />
 
--- textutils/src/components/Textform.js
+++ textutils/src/components/Textform.js
@@ -16,6 +16,20 @@ export default function Textform(props) {
         setText(newText);
     }
 
+    const handleCopy = () =>{
+        console.group("iam copy");
+        var text = document.getElementById("myBox"); 
+        text.select();
+        text.setSelectionRange(0, 9999)
+        navigator.clipboard.writeText(text.value);
+
+    }
+
+    const handleExtraSpaces = () =>{
+        let newText = text.split(/[ ]+ /);
+        setText(newText.join(" "))
+    }
+
 
     const handleOnChange =(event) =>{
         setText(event.target.value);
@@ -26,26 +40,30 @@ export default function Textform(props) {
     // setText('new text');
     return (
         <>
-        <div className='container'>
+        <div className='container'  style={{color: props.mode === 'dark'? 'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
-                <textarea className="form-control" value={text} onChange={handleOnChange}  id="myBox" rows="8"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'? '#4b5258':'white', color: props.mode === 'dark'? 'white':'black'}}  id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2" onClick={handleClickUp}>Convert to uppercase</button>
         
             <button className="btn btn-primary mx-2" onClick={handleClickdwn}>Convert to Lowecase</button>
 
-            <button className="btn btn-primary" onClick={handleclear}>Clear Text</button>
+            <button className="btn btn-primary mx-2" onClick={handleclear}>Clear Text</button>
+
+            <button className="btn btn-primary mx-3" onClick={handleCopy}>Copy Text</button>
+
+            <button className="btn btn-primary mx-2 my-3" onClick={handleExtraSpaces}>Remove extra spaces</button>
 
 
         </div>
-        <div className="container">
+        <div className="container" style={{color: props.mode === 'dark'? 'white':'black'}}>
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
 
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008* text.split(" ").length} Minutes Read </p>
-            <h3>Preview</h3> <p>{text} </p>
+            <h3>Preview</h3> <p>{text.length>0 ? text : "Enter text to Preview" } </p>
         </div>
         </>
     )
