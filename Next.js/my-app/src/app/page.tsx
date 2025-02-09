f12f45c8ffcbe8c219dc7fc6b227687e4f33f2a4
Tiyara <tiara26dia37@gmail.com>
2025-02-09 09:13:55 +0530

    more sections in next.js

--- Next.js/my-app/src/app/page.tsx
+++ Next.js/my-app/src/app/page.tsx
@@ -1,5 +1,8 @@
 import Featuredcourses from "@/components/Featuredcourses";
 import Hero from "@/components/Hero";
+import MusicTestimonial from "@/components/MusicTestimonial";
+import Webinar from "@/components/Webinar";
+import WhyChoose from "@/components/WhyChoose";
 
 export default function Home() {
   return (
@@ -7,6 +10,9 @@ export default function Home() {
       <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] text-white">
         <Hero/>
         <Featuredcourses/>
+        <WhyChoose/>
+        <MusicTestimonial/>
+        <Webinar/>
       </main>
     </div>
   );
