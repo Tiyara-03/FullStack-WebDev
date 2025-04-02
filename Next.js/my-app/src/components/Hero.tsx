7ec4cee88770f843ac0ce958526abb5cc8bdb5f6
Tiyara <tiara26dia37@gmail.com>
2025-04-02 07:09:18 +0530

    ..

--- Next.js/my-app/src/components/Hero.tsx
+++ Next.js/my-app/src/components/Hero.tsx
@@ -9,6 +9,9 @@ export default function Hero() {
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="blue"
       />
+      <div className="absolute top-3 left-2 inset-0">
+        <img src="/chromecast.png" alt="na" />
+      </div>
       <div className="p-4 z-10 relative w-full text-center">
             <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text bg-gradient-to-b from-neutral-50 to-neutral-400 text-transparent">Master the art of music</h1>
             <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi sequi saepe iure, repudiandae commodi ad qui officiis voluptatum? Iste quisquam laudantium, veritatis tenetur reprehenderit quo expedita impedit illum nulla debitis?</p>
